6f9470bb465991a80d3c9ddb5ad0120aed9f34cd
Mattia-Zanini <mattzanini2@gmail.com>
2021-12-08 20:34:02 +0100

    GetList()
    si occupa di popolare la lista dei prodotti all'avvio dell'app

--- Candy/Program.cs
+++ Candy/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -13,8 +14,10 @@ namespace Candy
     {
         public static List<Prodotto> products = new List<Prodotto>();
         public static bool modifiedProducts = true;
+        public static string path = AppDomain.CurrentDomain.BaseDirectory + "Magazzino.txt";
         public static void Main(string[] args)
         {
+            GetList();
             CreateHostBuilder(args).Build().Run();
         }
 
@@ -24,5 +27,15 @@ namespace Candy
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+        private static void GetList()
+        {
+            List<string> lines = new List<string>();
+            lines = File.ReadAllLines(path).ToList();
+            foreach (string line in lines)
+            {
+                string[] item = line.Split(',');
+                products.Add(new Prodotto { nome = item[0], quantita = Convert.ToInt32(item[1]), prezzo = Convert.ToDouble(item[2]) });
+            }
+        }
     }
 }
