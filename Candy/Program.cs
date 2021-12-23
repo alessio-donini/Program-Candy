5245313651c308ba9af57e855f925d18f76ed407
Mattia-Zanini <mattzanini2@gmail.com>
2021-12-23 09:07:38 +0100

    per renderlo funzionante anche su mac

--- Candy/Program.cs
+++ Candy/Program.cs
@@ -15,9 +15,10 @@ namespace Candy
         public static bool modifiedProducts = false;
         public static string path = AppDomain.CurrentDomain.BaseDirectory;
         public static Prodotto p = new Prodotto();
+        public static bool isWindows = true;
         public static void Main(string[] args)
         {
-            CleanPath();
+            Clean();
             p.Disponibilita(ref products, ref path, ref modifiedProducts);
             CreateHostBuilder(args).Build().Run();
         }
@@ -37,9 +38,17 @@ namespace Candy
                 modifiedProducts = false;
             }
         }
-        private static void CleanPath()//pulisce il percorso del programma
+        private static void Clean()
         {
-            string[] tmp = path.Split('\\');//divide l'array per ogni \ che divide la stringa
+            GetOS();
+            if (isWindows)
+                CleanPath('\\');
+            else
+                CleanPath('/');
+        }
+        private static void CleanPath(char toRemove)//pulisce il percorso del programma
+        {
+            string[] tmp = path.Split(toRemove);//divide l'array per ogni \ che divide la stringa
             var tmpList = tmp.ToList();//crea una lista dove inserisce i valori dell'array e per farlo lo converte in una lista
             tmpList.Remove("netcoreapp3.1");//rimuove la stringa passata per parametro alla funzione, dalla lista
             tmpList.Remove("Debug");//rimuove la stringa passata per parametro alla funzione, dalla lista
@@ -48,9 +57,15 @@ namespace Candy
             path = tmp[0];//inizializza la variabile "outPath" con il primo elemento dell'array "tmp"
             for (int i = 1; i < tmp.Length; i++)//ricompone il percorso assoluto
             {
-                path += $"\\{tmp[i]}";//aggiunge un pezzo di stringa
+                path += $"{toRemove}{tmp[i]}";//aggiunge un pezzo di stringa
             }
             path += "Magazzino.txt"; // aggiunge il nome del file txt da leggere alla fine del percorso
+        }
+        private static void GetOS()
+        {
+            string os = Environment.OSVersion.ToString().ToLower();
+            if (os.Contains("mac"))
+                isWindows = false;
         }
     }
 }
