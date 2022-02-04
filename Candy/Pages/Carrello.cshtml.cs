0bdbf57419d48caf6cba61d3326d8b52a1bf6ad0
Mattia-Zanini <mattzanini2@gmail.com>
2022-02-04 09:09:32 +0100

    creata la pagina del carrello

---
+++ Candy/Pages/Carrello.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Candy.Pages
+{
+    public class CarrelloModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
