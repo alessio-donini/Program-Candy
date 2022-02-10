30da4b4eac9943df04a47f9cfb969e9fa6561033
diegoalbertin <albertindiego08@gmail.com>
2022-02-10 09:50:00 +0100

    .

--- Candy/Pages/Accesso.cshtml.cs
+++ Candy/Pages/Accesso.cshtml.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Candy.Pages
@@ -11,6 +6,10 @@ namespace Candy.Pages
     {
         public void OnGet()
         {
+        }
+        public void OnPost(string email, string pass)
+        {
+            System.Console.WriteLine("ciao");
         }
     }
 }
